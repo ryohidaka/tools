b82627124a1360ee0a4e72b995d4870ae6d7fc9a
ryohidaka <git@ryohidaka.jp>
2021-05-05 20:25:29 +0900

    fix: モックアプ追加に伴うレイアウト修正

--- src/MailTo.tsx
+++ src/MailTo.tsx
@@ -5,12 +5,18 @@ import TextField from "@material-ui/core/TextField";
 
 import { Grid, Typography, Button } from "@material-ui/core";
 
+import Mockup from "./Mockup";
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       "& > *": {
         margin: theme.spacing(1),
       },
+    },
+    paper: {
+      width: "100%",
+      height: "100%",
     },
   })
 );
@@ -44,41 +50,65 @@ export default function MailTo() {
   }
 
   return (
-    <Grid container direction="column" justify="center" alignItems="center">
-      <form className={classes.root} noValidate autoComplete="off">
-        <TextField
-          id="email"
-          label="送信先メールアドレス"
-          type="email"
-          variant="outlined"
-          name="email"
-          onChange={handleInputChange}
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          id="subject"
-          label="指定する件名"
-          type="text"
-          variant="outlined"
-          name="subject"
-          onChange={handleInputChange}
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          id="body"
-          label="指定する本文"
-          type="text"
-          variant="outlined"
-          name="body"
-          onChange={handleInputChange}
-          multiline
-          rowsMax={10}
-          fullWidth
-          margin="normal"
-        />
-      </form>
+    <Grid container direction="column" className={classes.root}>
+      <Grid container direction="row">
+        <Grid
+          container
+          item
+          direction="column"
+          justify="center"
+          alignItems="center"
+          xs={12}
+          md={6}
+        >
+          <form noValidate autoComplete="off">
+            <TextField
+              id="email"
+              label="送信先メールアドレス"
+              type="email"
+              variant="outlined"
+              name="email"
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              id="subject"
+              label="指定する件名"
+              type="text"
+              variant="outlined"
+              name="subject"
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              id="body"
+              label="指定する本文"
+              type="text"
+              variant="outlined"
+              name="body"
+              onChange={handleInputChange}
+              multiline
+              rowsMax={10}
+              fullWidth
+              margin="normal"
+            />
+          </form>
+        </Grid>
+
+        <Grid
+          container
+          item
+          justify="center"
+          alignItems="center"
+          xs={12}
+          md={6}
+        >
+          <Mockup values={values} />
+        </Grid>
+      </Grid>
+
       <Typography variant="h5" component="p">
         {url}
       </Typography>
